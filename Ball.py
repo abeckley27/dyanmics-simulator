e19433d2bfff603fc16242832e044b34df9fc770
abeckley27 <aidanbeckley@gmail.com>
2023-08-05 17:24:24 -0400

    Fixed wall collisions

--- Ball.py
+++ Ball.py
@@ -30,18 +30,23 @@ class Ball(object):
     def some_inside(self, maxx, maxy):
         return 0 < self.x + self.radius and self.x - self.radius < maxx and 0 < self.y + self.radius and self.y - self.radius < maxy
 
+    #For checking collisions with walls
     def check_and_reverse(self, maxx, maxy):
         if self.x <= 0: 
             self.vx = self.vx * -1
+            self.x = abs(self.x)
             
         if self.x >= maxx:
             self.vx = self.vx * -1
+            self.x = 2 * maxx - self.x
             
         if self.y <= 0:
             self.vy = self.vy * -1
+            self.y = abs(self.y)
             
         if self.y >= maxy:
             self.vy = self.vy * -1
+            self.y = 2 * maxy - self.y
 
     def velocity(self):
         return (self.vx, self.vy)
